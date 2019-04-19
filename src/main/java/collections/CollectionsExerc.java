5adcaa28e5a65d3a33ceecd028c9fef8bba1cb6f
DawidMk <dawid_m@interia.pl>
2019-04-19 19:28:45 +0200

    maps added

--- src/main/java/collections/CollectionsExerc.java
+++ src/main/java/collections/CollectionsExerc.java
@@ -4,6 +4,7 @@ package collections;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class CollectionsExerc {
@@ -34,10 +35,63 @@ public class CollectionsExerc {
         return sortedAnimals().stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList());
     }
 
+    public static List<String> sortedAnimalsDistinct() {
+        List<String> result = new ArrayList<>();
+        for (String animal : noBlankAnimals()) {
+            if (!result.contains(animal)) {
+                result.add(animal);
+            }
+        }
+        Collections.sort(result);
+//        result.sort(Comparator.naturalOrder());
+        return result;
+    }
+
+
+    public static List<String> sortedAnimalsDistinctStream() {
+        return sortedAnimals().stream().sorted(Comparator.naturalOrder()).distinct().collect(Collectors.toList());
+    }
+
+    public static List<String> originalAnimalsDistinct() { //todo napisać test
+        List<String> result = new ArrayList<>();
+        for (String animal : noBlankAnimals()) {
+            if (!result.contains(animal)) {
+                result.add(animal);
+            }
+        }
+        return result;
+    }
+
+
+    public static List<String> originalAnimalsDistinctStream() { //todo napisać test
+        return noBlankAnimals().stream().distinct().collect(Collectors.toList());
+    }
+
+    public static Map<String, Long> animalMap() {
+        Map<String, Long> result = new HashMap<>();
+        for (String animal : noBlankAnimals()) {
+            if (result.containsKey(animal)) {
+                result.put(animal, result.get(animal) + 1);
+            }else{
+                result.put(animal,1L);
+            }
+        }return result;
+    }
+
+    public static Map<String, Long> animalMapStream() {
+        return noBlankAnimals().stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+    }
 
 
     public static void main(String[] args) {
-        System.out.println(sortedAnimals().toString());
-        System.out.println(sortedAnimalsStream().toString());
+        System.out.println(Arrays.toString(animals));
+//        System.out.println(sortedAnimals().toString());
+//        System.out.println(sortedAnimalsDistinct().toString());
+//        System.out.println(sortedAnimalsDistinctStream().toString());
+        System.out.println(originalAnimalsDistinct().toString());
+        System.out.println(originalAnimalsDistinctStream().toString());
+        System.out.println(animalMap());
+        System.out.println(animalMapStream());
 
-}}
+    }
+}
--- src/test/java/collections/CollectionsExercTest.java
+++ src/test/java/collections/CollectionsExercTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,5 +22,22 @@ class CollectionsExercTest {
     void noBlankAnimalsStream() {
         List<String> animalList = Arrays.asList("cat","dog","mouse","rat","pig","rabbit","hamster","parrot","cat","dog","cat","pig","dog");
         Assertions.assertEquals(CollectionsExerc.noBlankAnimalsStream(),animalList);
+    }
+
+    @Test
+    void animalMap() {
+        Map<String, Long> animalMap = CollectionsExerc.animalMap();
+        List<String> animalList = Arrays.asList("cat","dog","mouse","rat","pig","rabbit","hamster","parrot","cat","dog","cat","pig","dog");
+
+Assertions.assertTrue(animalMap.containsKey("dog") && animalMap.containsValue(3L));
+
+    }
+
+    @Test
+    void animalMapStream() {
+        Map<String, Long> animalMap = CollectionsExerc.animalMapStream();
+        List<String> animalList = Arrays.asList("cat","dog","mouse","rat","pig","rabbit","hamster","parrot","cat","dog","cat","pig","dog");
+
+
     }
 }
